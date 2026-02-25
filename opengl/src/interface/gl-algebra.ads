9461ea5e6b6541a134e2c04ffd7ece5f32967354
Andrew <amengede27@outlook.com>
2026-02-25 21:17:39 +1000

    Added documentation: gl-algebra

--- opengl/src/interface/gl-algebra.ads
+++ opengl/src/interface/gl-algebra.ads
@@ -29,22 +29,46 @@ package GL.Algebra is
    package Vectors4 is new Vectors (Index_Type   => Index_Homogeneous,
                                     Element_Type => Element_Type);
 
+   --  Two Dimensional Vector
    type Vector2 is new Vectors2.Vector;
+   --  Three Dimensional Vector
    type Vector3 is new Vectors3.Vector;
+   -- Four Dimensional Vector
    type Vector4 is new Vectors4.Vector;
 
-   -- Conversion functions between vectors. Default values are:
-   -- Z = 0, W = 1
+   -- Truncate a 3D Vector to a 2D Vector
+   -- @param Vector Vector to truncate
+   -- @return Truncated result
    function To_Vector2 (Vector : Vector3) return Vector2;
+   -- Truncate a 4D Vector to a 2D Vector
+   -- @param Vector Vector to truncate
+   -- @return Truncated result
    function To_Vector2 (Vector : Vector4) return Vector2;
+   -- Extend a 2D Vector to a 3D Vector, appending Z = 0.
+   -- @param Vector Vector to extend
+   -- @return Extended result
    function To_Vector3 (Vector : Vector2) return Vector3;
+   -- Truncate a 4D Vector to a 3D Vector
+   -- @param Vector Vector to truncate
+   -- @return Truncated result
    function To_Vector3 (Vector : Vector4) return Vector3;
+   -- Extend a 2D Vector to a 4D Vector, appending Z = 0, W = 1
+   -- @param Vector Vector to extend
+   -- @return Extended result
    function To_Vector4 (Vector : Vector2) return Vector4;
+   -- Extend a 3D Vector to a 4D Vector, appending W = 1
+   -- @param Vector Vector to extend
+   -- @return Extended result
    function To_Vector4 (Vector : Vector3) return Vector4;
-
    pragma Inline (To_Vector2);
    pragma Inline (To_Vector3);
    pragma Inline (To_Vector4);
+
+   -- Vector Cross Product
+   -- @param Left First Vector
+   -- @param Right Second Vector
+   -- @return the vector cross product (Left x Right)
+   function Cross_Product (Left, Right : Vector3) return Vector3;
 
    -----------------------------------------------------------------------------
    --                              Matrix types                               --
@@ -60,14 +84,19 @@ package GL.Algebra is
                                       Element_Type => Element_Type,
                                       Vector_Type  => Vector4);
 
+   -- 2x2 Matrix
    type Matrix2 is new Matrices2.Matrix;
+   -- 3x3 Matrix
    type Matrix3 is new Matrices3.Matrix;
+   -- 4x4 Matrix
    type Matrix4 is new Matrices4.Matrix;
-
+   -- 2x2 Identity Matrix
    Identity2 : constant Matrix2 := ((One_Value, Null_Value), (Null_Value, One_Value));
+   -- 3x3 Identity Matrix
    Identity3 : constant Matrix3 := ((One_Value, Null_Value, Null_Value),
                                     (Null_Value, One_Value, Null_Value),
                                     (Null_Value, Null_Value, One_Value));
+   -- 4x4 Identity Matrix
    Identity4 : constant Matrix4 := ((One_Value, Null_Value, Null_Value, Null_Value),
                                     (Null_Value, One_Value, Null_Value, Null_Value),
                                     (Null_Value, Null_Value, One_Value, Null_Value),
@@ -77,12 +106,18 @@ package GL.Algebra is
    --                               Array types                               --
    -----------------------------------------------------------------------------
 
+   -- Array of 2D Vectors
    type Vector2_Array is array (Index_Type range <>) of aliased Vector2;
+   -- Array of 3D Vectors
    type Vector3_Array is array (Index_Type range <>) of aliased Vector3;
+   -- Array of 4D Vectors
    type Vector4_Array is array (Index_Type range <>) of aliased Vector4;
 
+   -- Array of 2x2 Matrices
    type Matrix2_Array is array (Index_Type range <>) of aliased Matrix2;
+   -- Array of 3x3 Matrices
    type Matrix3_Array is array (Index_Type range <>) of aliased Matrix3;
+   -- Array of 4x4 Matrices
    type Matrix4_Array is array (Index_Type range <>) of aliased Matrix4;
 
    pragma Convention (C, Vector2_Array);
@@ -111,6 +146,4 @@ package GL.Algebra is
      (Index_Type, Matrix3, Matrix3_Array, Matrix3'(others => (others => <>)));
    package Matrix4_Pointers is new Interfaces.C.Pointers
      (Index_Type, Matrix4, Matrix4_Array, Matrix4'(others => (others => <>)));
-
-   function Cross_Product (Left, Right : Vector3) return Vector3;
 end GL.Algebra;
